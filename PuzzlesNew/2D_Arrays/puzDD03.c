466add8586c0582e0788919f48885ca1359865c1
Antonio Zea Jr <spark343@pop-os.localdomain>
2020-09-29 21:05:15 -0400

    Wrote a function that prints the elements of a 2D integer array.

--- PuzzlesNew/2D_Arrays/puzDD03.c
+++ PuzzlesNew/2D_Arrays/puzDD03.c
@@ -1,15 +1,29 @@
 #include <stdio.h>
-void print2DArray(int nrows, int ncols, int x[][ncols]){
-    int i;
-    for(i = 0; i < nrows*ncols; i++){
-        if(i%ncols == ncols-1){
-            printf("%d\n",**(x+i));
+/*
+ * Write a function that prints the elements of a 2D integer array. Write one
+ * row the the array per line on the screen. 
+ */
+
+void print2DArray(int nrows, int ncols, int x[nrows][ncols]){
+    int r, c;
+    for(r = 0; r < nrows; r++){
+        for(c = 0; c < ncols; c++){
+            /*
+             * Doing this with pointers just makes it harder to read not sure if
+             * there are any performace differences.
+             * printf("%4d ",*(*(x+r)+c));
+             *
+             * The following seems more readable imho.
+             */
+            printf("%4d ", x[r][c]);
         }
+        printf("\n");
     }
 }
 
 #define COLS 15
 #define ROWS 5
+
 int main(int argc, const char *argv[]){
     int x[ROWS][COLS];
     int count = 0;
