89449ea0e3d16ff48e96bc40ea6ccdcab604f67c
Antonio Zea Jr <halosprtn117@gmail.com>
2020-12-24 23:01:05 -0500

    Call by value struct

---
+++ PuzzlesNew/T_Structs/puzT03.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+struct Bulb{
+    int watts;
+    int lumens;
+};
+
+void printBulb(struct Bulb b){
+    printf("Bulb: watts = %d\tlumens = %d\n",   b.watts, b.lumens );
+}
+
+int main(int argc, char const *argv[]){
+    struct Bulb bulbA, bulbB;
+    bulbA.watts = 100; bulbA.lumens = 1710;
+    bulbB.watts =  60; bulbB.lumens = 1065;
+
+    printBulb(bulbA);
+    printBulb(bulbB);
+
+
+
+    
+    return 0;
+}
